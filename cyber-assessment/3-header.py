633b8f6ccab14f51e53134d9b019ac4f86db90da
Ihor Mac <kramarets1hor@gmail.com>
2026-01-15 12:25:18 +0100

    3

--- cyber-assessment/3-header.py
+++ cyber-assessment/3-header.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-import urllib, sys
-# get url, for example https://httpbin.org/response-headers?X-Request-Id=hello_world and print only the value of X-Request-Id, using a with statement
+import urllib.request, sys
 url = sys.argv[1]
 with urllib.request.urlopen(url) as response:
-    headers = response.getheaders()
-    for header in headers:
-        if header[0].lower() == 'x-request-id':
-            print(header[1])
+    print(response.headers.get("X-Request-Id"))
