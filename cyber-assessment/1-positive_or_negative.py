5022eaa70dd9221c8b7dd6f9dd7eeac2571066e2
Ihor Mac <kramarets1hor@gmail.com>
2026-01-15 11:54:24 +0100

    1

---
+++ cyber-assessment/1-positive_or_negative.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+# YOUR CODE HERE
+if number >= 0: 
+    print(str(number) + " is positive\n")
+elif number < 0:
+    print(str(number) + " is negative\n")
+else: 
+    print(str(number) + " is zero\n")
