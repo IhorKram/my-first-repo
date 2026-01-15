b3e76b40fbf36a6f73a7ec63af1c5170911e1de0
Ihor Mac <kramarets1hor@gmail.com>
2026-01-15 13:01:09 +0100

    5

--- cyber-assessment/5-my_github.py
+++ cyber-assessment/5-my_github.py
@@ -6,6 +6,9 @@ url = "https://api.github.com/user"
 username = sys.argv[1]
 password = sys.argv[2]
 response = requests.get(url, auth=(username, password))
-print(response.json()["id"])
+if response.status_code == 200:
+    print(response.json()["id"])
+else:
+    print("None")
 
 
