ccc7867379134f5fd1ab65e9d53d5fba52fa231b
Ihor Mac <kramarets1hor@gmail.com>
2026-01-15 12:59:29 +0100

    5

---
+++ cyber-assessment/5-my_github.py
@@ -0,0 +1,11 @@
+# !/usr/bin/python3
+import sys
+import requests
+# get and print my github id with username and password passed as arguments
+url = "https://api.github.com/user"
+username = sys.argv[1]
+password = sys.argv[2]
+response = requests.get(url, auth=(username, password))
+print(response.json()["id"])
+
+
